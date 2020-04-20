2e2108bc9e7c197fe17769eb8c00e00a657960df
S Yashaswi <ysanjeevaiah@altimetrik.com>
2020-04-20 17:56:44 +0530

    updated get mapping

--- src/main/java/com/springboot/Library_management/library/LibraryController.java
+++ src/main/java/com/springboot/Library_management/library/LibraryController.java
@@ -1,11 +1,10 @@
 package com.springboot.Library_management.library;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class LibraryController {
@@ -23,22 +24,36 @@ public class LibraryController {
 	
 	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping(path="/books")
-	public ResponseEntity<Books> saveLibrary(@RequestBody Books library) {
-		Books libraryobj= libraryrepo.save(library);
-		return new ResponseEntity<Books>(libraryobj, HttpStatus.OK);
+	public Books addBooks(@RequestBody Books book) {
+		 libraryrepo.save(book);
+		return book; 
 	}
+	
+//	@CrossOrigin(origins = "http://localhost:3000")
+//	@GetMapping(path="/books")
+//	public List<Books> getAllBooks()
+//	{
+//		List<Books> book =new ArrayList<>();
+//		libraryrepo.findAll().forEach(book :: add);
+//	    return book;
+//	}
 	
 	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping(path="/books")
-	@ResponseBody
-	public List<Books> getLibrary() {
+	public List<Books> getAllBooks() {
 		return libraryrepo.findAll();
-		
+	}
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping("/books/{bookId}")
+	public Books getBooks(@PathVariable int bookId)
+	{
+	   Optional<Books> libraryOpt  = libraryrepo.findById(bookId);
+	   return libraryOpt.orElseThrow(() -> new RuntimeException("Data not found"));
 	}
 	
 	@CrossOrigin(origins = "http://localhost:3000")
 	@DeleteMapping("/books/{bookId}")
-	public void deleteLibrary( @PathVariable int bookId ){
+	public void deleteBooks( @PathVariable int bookId ){
 		  libraryrepo.deleteById(bookId);	
 	}
 	
